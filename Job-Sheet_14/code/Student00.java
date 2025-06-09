bb43321912abeff19a04ba0b1fe8e560127c922f
Joseph Atem Deng <atemhatingaa@gmail.com>
2025-06-09 17:38:22 +0700

    Add files via upload

---
+++ Job-Sheet_14/code/Student00.java
@@ -0,0 +1,14 @@
+public class Student00 {
+    String nim, name, className;
+    double ipk;
+
+    Student00(String nim, String name, String className, double ipk){
+        this.nim = nim;
+        this.name = name;
+        this.className = className;
+        this.ipk = ipk;
+    }
+    void print(){
+        System.out.println(nim+" - "+name+" - "+className+" - "+ipk);
+    }
+}
